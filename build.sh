29ff56a4105b97ccb0dce4738efa4640f3cad697
afnrfcb7 <emory.merryman@gmail.com>
2015-11-27 09:57:53 +0000

    make repeatable by working in original directory outside git clone

--- build.sh
+++ build.sh
@@ -7,8 +7,9 @@ then
 fi &&
 cd leiningen &&
 git checkout tags/2.5.3 &&
+cd .. &&
 mkdir --parents leiningen_2.5.3/usr/bin &&
-cp bin/lein leiningen_2.5.3/usr/bin &&
+cp leiningen/bin/lein leiningen_2.5.3/usr/bin &&
 chmod 0555 leiningen_2.5.3/usr/bin/lein &&
 mkdir --parents leiningen_2.5.3/DEBIAN/control &&
 cp ../control leiningen_2.5.3/DEBIAN/control &&
