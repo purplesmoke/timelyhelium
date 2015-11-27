dc22eb3b1d7243745a345606440cd60740d0c412
afnrfcb7 <emory.merryman@gmail.com>
2015-11-27 17:50:12 +0000

    use mine

--- build.sh
+++ build.sh
@@ -8,12 +8,12 @@ fi &&
 cd leiningen &&
 git checkout tags/2.5.3 &&
 cd .. &&
-mkdir --parents leiningen_2.5.3/usr/bin &&
-cp leiningen/bin/lein leiningen_2.5.3/usr/bin &&
-chmod 0555 leiningen_2.5.3/usr/bin/lein &&
-mkdir --parents leiningen_2.5.3/DEBIAN &&
-cp control leiningen_2.5.3/DEBIAN/control &&
-dpkg-deb --build leiningen_2.5.3 &&
+mkdir --parents myleiningen_2.5.3/usr/bin &&
+cp leiningen/bin/lein myleiningen_2.5.3/usr/bin &&
+chmod 0555 myleiningen_2.5.3/usr/bin/lein &&
+mkdir --parents myleiningen_2.5.3/DEBIAN &&
+cp control myleiningen_2.5.3/DEBIAN/control &&
+dpkg-deb --build myleiningen_2.5.3 &&
 if [ ! -d eternalmetaphor ]
 then
 git clone git@github.com:rawflag/eternalmetaphor.git &&
@@ -21,7 +21,7 @@ true
 fi &&
 cd eternalmetaphor &&
 mkdir --parents amd64 &&
-cp ../leiningen_2.5.3.deb amd64 &&
+cp ../myleiningen_2.5.3.deb amd64 &&
 dpkg-scanpackages amd64 /dev/null | gzip -9c > amd64/Packages.gz &&
 git add amd64 &&
 git commit -m "added leiningen_2.5.3" &&
