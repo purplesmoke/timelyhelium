a459f1d661e178e42e3a682563e78d19e60292ae
afnrfcb7 <emory.merryman@gmail.com>
2015-11-27 10:37:13 +0000

    create repo

--- build.sh
+++ build.sh
@@ -14,4 +14,15 @@ chmod 0555 leiningen_2.5.3/usr/bin/lein &&
 mkdir --parents leiningen_2.5.3/DEBIAN &&
 cp control leiningen_2.5.3/DEBIAN/control &&
 dpkg-deb --build leiningen_2.5.3 &&
+if [ ! -d eternalmetaphor ]
+then
+git clone git@github.com:rawflag/eternalmetaphor.git &&
+true &&
+fi &&
+cd eternalmethaphor &&
+cp ../leiningen_2.5.3.deb . &&
+dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz &&
+git add leiningen_2.5.3.deb Packages.gz &&
+git commit -m "added leiningen_2.5.3" &&
+git push origin master &&
 true
