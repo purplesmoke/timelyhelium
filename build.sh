6dca0ac99698ff3a48180bda163debb815e3c6ad
afnrfcb7 <emory.merryman@gmail.com>
2015-11-27 09:55:41 +0000

    make repeatable

--- build.sh
+++ build.sh
@@ -11,6 +11,6 @@ mkdir --parents leiningen_2.5.3/usr/bin &&
 cp bin/lein leiningen_2.5.3/usr/bin &&
 chmod 0555 leiningen_2.5.3/usr/bin/lein &&
 mkdir --parents leiningen_2.5.3/DEBIAN/control &&
-cp control leiningen_2.5.3/DEBIAN/control &&
+cp ../control leiningen_2.5.3/DEBIAN/control &&
 dpkg-deb --build leiningen_2.5.3 &&
 true
