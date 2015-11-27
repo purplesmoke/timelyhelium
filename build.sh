72912441a795116617633f5acdd0b9a201643c4b
afnrfcb7 <emory.merryman@gmail.com>
2015-11-27 09:53:53 +0000

    make repeatable

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-git clone git@github.com:dirtyfrostbite/leiningen.git &&
+if [ ! -d leiningen ]
+then
+    git clone git@github.com:dirtyfrostbite/leiningen.git &&
+    true
+fi &&
 cd leiningen &&
 git checkout tags/2.5.3 -b 2.5.3 &&
 mkdir --parents leiningen_2.5.3/usr/bin &&
 cp bin/lein leiningen_2.5.3/usr/bin &&
 chmod 0555 leiningen_2.5.3/usr/bin/lein &&
-mkdir leiningen_2.5.3/DEBIAN/control &&
+mkdir --parents leiningen_2.5.3/DEBIAN/control &&
 cp control leiningen_2.5.3/DEBIAN/control &&
 dpkg-deb --build leiningen_2.5.3 &&
 true
