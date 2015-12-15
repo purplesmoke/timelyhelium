9e07e66fc657f11160e019dd9e906478ac76403a
afnrfcb7 <emory.merryman@gmail.com>
2015-12-15 11:55:20 +0000

    it seems to work, but the template is wrong

--- usage.sh
+++ usage.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-sudo su --login <<EOF
+(sudo su --login <<EOF
     apt-get install --assume-yes apt-transport-https &&
     echo deb https://raw.githubusercontent.com/rawflag/eternalmetaphor/master/ main amd64 > /etc/apt/sources.list.d/eternalmetaphor.list &&
     ( apt-get update || true) &&
-    apt-get install --assume-yes --force-yes timelyhelium
+    apt-get install --assume-yes --force-yes timelyhelium &&
+    true
 EOF
+) &&
+cd $(mktemp -d) &&
+git clone git@github.com:sillyflannel/easterntombstone.git &&
+cd easterntombstone &&
+lein install &&
+true
