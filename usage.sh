353333da70543d5ca71729d1e1b694a73c6b3595
afnrfcb7 <emory.merryman@gmail.com>
2015-12-15 11:15:04 +0000

    added readme usage that works

--- usage.sh
+++ usage.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 sudo su --login <<EOF
-    apt-get install apt-transport-https &&
+    apt-get install --assume-yes apt-transport-https &&
     echo deb https://raw.githubusercontent.com/rawflag/eternalmetaphor/master/ main amd64 > /etc/apt/sources.list.d/eternalmetaphor.list &&
     ( apt-get update || true) &&
-    apt-get install timelyhelium
+    apt-get install --assume-yes --force-yes timelyhelium
 EOF
